0f822db8a1f1becc254c7f81303001695b4b2232
Vishal Polley <vishal@sureify.com>
2019-07-07 22:44:57 +0530

    Handling Events

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,28 +2,35 @@ import React, { Component } from "react";
 
 class Counter extends Component {
   state = {
-    count: 1,
-    tags: []
+    count: 1
   };
 
-  renderTags() {
-    if (this.state.tags.length === 0) return <p>There are no tags!</p>;
-    return (
-      <ul>
-        {this.state.tags.map(tag => (
-          <li key={tag}>{tag}</li>
-        ))}
-      </ul>
-    );
+  handleIncrement() {
+    console.log("Increment Clicked");
   }
 
   render() {
     return (
       <React.Fragment>
-        {this.state.tags.length === 0 && "Please create a new tag!"} // Truthy
-        {this.renderTags()}
+        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+        <button
+          onClick={this.handleIncrement} // Only reference is passed
+          className="btn btn-secondary btn-sm"
+        >
+          Increment
+        </button>
       </React.Fragment>
     );
+  }
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    classes += this.state.count === 0 ? "warning" : "primary";
+    return classes;
+  }
+
+  formatCount() {
+    const { count } = this.state;
+    return count === 0 ? "Zero" : count;
   }
 }
 
