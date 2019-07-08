ea51806ba03557a6b8e0f7d3a38881b210e42bf6
Vishal Polley <vishal@sureify.com>
2019-07-08 12:13:31 +0530

    Lifting the State Up

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -4,9 +4,12 @@ class NavBar extends Component {
   render() {
     return (
       <nav className="navbar navbar-light bg-light">
-        <a className="navbar-brand" href="#">
-          Navbar
-        </a>
+        <span className="navbar-brand mb-0 h1">
+          Navbar{" "}
+          <span className="badge badge-pill badge-secondary">
+            {this.props.totalCounters}
+          </span>{" "}
+        </span>
       </nav>
     );
   }
