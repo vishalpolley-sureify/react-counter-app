aef0e5b6715003f4f1abf3b7859fb7d79c1f989f
Vishal Polley <vishal@sureify.com>
2019-07-08 11:30:24 +0530

    Single Source of Truth

--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -11,14 +11,28 @@ class Counters extends Component {
     ]
   };
 
+  handleReset = () => {
+    const counters = this.state.counters.map(c => {
+      c.value = 0;
+      return c;
+    });
+    this.setState({ counters });
+  };
+
   handleDelete = counterId => {
     const counters = this.state.counters.filter(c => c.id !== counterId);
-    this.setState({ counters: counters });
+    this.setState({ counters });
   };
 
   render() {
     return (
       <div>
+        <button
+          onClick={this.handleReset}
+          className="btn btn-primary btn-sm m-2"
+        >
+          Reset
+        </button>
         {this.state.counters.map(counter => (
           <Counter
             key={counter.id}
